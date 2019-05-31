6c4d6ba5e7592ff33bd0bf2ed45b3b34bab98f56
Wonjae Kim <wonjaek36@gmail.com>
2019-05-31 14:50:10 +0900

    Update main for automatic testing

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ import json
 import sys
 from parser import Parser
 from models.VGGNet import VGGNet
+from contextlib import redirect_stdout
 
 import numpy as np
 import tensorflow as tf
@@ -15,6 +16,7 @@ class Main():
 
     def parse_data(self): 
         
+        config = self.config
         data_parser = Parser(config)
         data, labels = data_parser.parsing()
         data_shape, label_shape = data_parser.get_data_description()
@@ -36,37 +38,99 @@ class Main():
 
         return model
 
-    def train(self):
-            
+
+    def train(self, options={}):
+
         model = self.get_model()
+
+        name = options.get("name", None)
+        epoch = options.get("epoch", 1000)
+        batch_size = options.get("batch_size", 32)
+        learning_rate = options.get("learning_rate", 0.0001)
+        ratio_train = options.get("train", 0.7)
+        ratio_valid = options.get("valid", 0.3)
+
         if model is None:
             return
 
         data_shape = self.data_shape
         output = self.label_shape
-        #tf.reset_default_graph()
-        #with tf.Session() as sess:
-            
-            # X, Y = model.create_placeholders(data_shape[0], data_shape[1], data_shape[2], output)
-            # parameters = model.initialize_parameters()
         model = model.create_keras_model()
+        optimizer = tf.train.AdamOptimizer(learning_rate = learning_rate)
+
         model.compile(
-            optimizer='adam',
+            optimizer=optimizer,
             loss='categorical_crossentropy',
             metrics=['accuracy'])
 
-        model.summary()
+        numTrain = int(self.num_data * ratio_train)
+        numValid = self.num_data - numTrain
+
+        x_train = self.data[0:numTrain]
+        y_train = self.labels[0:numTrain]
+        x_valid = self.data[numTrain:]
+        y_valid = self.labels[numTrain:]
+
+        if name is not None: 
+            # Create directory 
+            name = "TEST_" + name
+            if not os.path.exists(os.path.join('.', name)):
+                os.makedirs(os.path.join('.', name))
+
+            with open(os.path.join(name, 'model_summary.txt'), 'w') as f:
+                with redirect_stdout(f):
+                    model.summary()
 
 
-        x_train = self.data[0: int(self.num_data*0.7)]
-        y_train = self.labels[0: int(self.num_data*0.7)]
-        model.fit(x_train, y_train,
-            batch_size=32,
-            epochs=1,
-            #validation_data=(x_test, y_test),
-            shuffle=True)
+            checkpoint_path = name + "/cp.ckpt"
+            checkpoint_dir = os.path.dirname(checkpoint_path)
+            cp_callback = tf.keras.callbacks.ModelCheckpoint(checkpoint_path, 
+                                                 save_weights_only=True,
+                                                 verbose=1)
 
+            history = model.fit(x_train, y_train,
+                batch_size=batch_size,
+                epochs=epoch,
+                validation_data=(x_valid, y_valid),
+                shuffle=True,
+                callbacks = [cp_callback] )
+            
+            history = history.history # Getting history attributes from history instance
+            history_filename = "history_" + name + ".txt"
+            history_file = open(os.path.join(name, history_filename), 'w')
 
+            history_file.write(str(history['acc']))
+            history_file.write('\n')
+            history_file.write(str(history['loss']))
+            history_file.write('\n')
+            history_file.write(str(history['val_acc']))
+            history_file.write('\n')
+            history_file.write(str(history['val_loss']))
+            history_file.write('\n')
+
+            history_file.close()
+            
+        if name is None:
+            model.summary()
+            history = model.fit(x_train, y_train,
+                batch_size=batch_size,
+                epochs=epoch,
+                validation_data=(x_valid, y_valid),
+                shuffle=True)
+
+            history = history.history # Getting history attributes from history instance
+
+            print (history['acc'])
+            print (history['loss'])
+            print (history['val_acc'])
+            print (history['val_loss'])
+
+    def prediction():
+        pass
+
+    def load_model():
+        pass
+        # model.load_weights(checkpoint_path)
 
 if __name__ == "__main__":
     config = None
