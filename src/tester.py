26d619d5206e9b24b7dedbfcb185e30b5f7866ec
Wonjae Kim <wonjaek36@gmail.com>
2019-05-31 22:25:16 +0900

    Support MobileNet

--- src/tester.py
+++ src/tester.py
@@ -52,6 +52,50 @@ for test in test_set:
 		if name is None:
 			continue
 
+	if test_model == "MobileNet":
+
+		print ("test_model: ", "MobileNet")
+		learning_rate 		= test["learning_rate"]
+		batch_size 			= test["batch_size"]
+		epoch 				= test["epoch"]
+		regularization1 	= test.get("regularization1", None)
+		regularization2 	= test.get("regularization2", None)
+		regularization3 	= test.get("regularization3", None)
+		regularization4 	= test.get("regularization4", None)
+		regularization5 	= test.get("regularization5", None)
+		regularization6 	= test.get("regularization6", None)
+		regularization7 	= test.get("regularization7", None)
+		regularization8 	= test.get("regularization8", None)
+		regularization9 	= test.get("regularization9", None)
+		regularization10 	= test.get("regularization10", None)
+		regularization11 	= test.get("regularization11", None)
+		regularization12 	= test.get("regularization12", None)
+		regularization13 	= test.get("regularization13", None)
+		regularization14 	= test.get("regularization14", None)
+		name				= test.get("name", None)
+
+		options = {
+			"learning_rate" 		:learning_rate 	,
+			"batch_size" 			:batch_size 	,
+			"epoch" 				:epoch 			,
+			"regularization1" 		:regularization1,
+			"regularization2" 		:regularization2,
+			"regularization3" 		:regularization3,
+			"regularization4" 		:regularization4,
+			"regularization5" 		:regularization5,
+			"regularization6" 		:regularization6,
+			"regularization7" 		:regularization7,
+			"regularization8" 		:regularization8,
+			"regularization9" 		:regularization9,
+			"regularization10" 		:regularization10,
+			"regularization11" 		:regularization11,
+			"regularization12" 		:regularization12,
+			"regularization13" 		:regularization13,
+			"regularization14" 		:regularization14,
+			"name"					:name
+		}
+
+
 
 	main = Main(config)
 	main.parse_data()
